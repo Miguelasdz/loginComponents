f67992c6b3b478512a59224abd6e3d4d572dc65a
Miguel Angel Hernandez Espinoza <miguel.hernandeze@enginecore.com.mx>
2024-03-22 14:44:35 -0600

    first commit

---
+++ src/components/input/input-component.js
@@ -0,0 +1,99 @@
+class inputComponent extends HTMLElement{ //cambiar el nombre del titulo inputComponent
+    //constructor
+    constructor(){
+        super() //investigar super
+        this.attachShadow({mode: "open"})
+        this.inputC=document.createElement("input")
+        this.labelC=document.createElement("label")
+    }
+
+    //estilos
+    getTemplate(){
+        const template=document.createElement("template")
+        template.innerHTML=
+        `<style>
+        
+        label{
+            text-align:center;
+            align-items:center;
+            font-size: 1rem;
+            line-height: 1.25rem;
+            color: rgb(255 255 255);
+            padding-bottom: 10%;
+            
+        }
+        
+        input{
+            margin: 0;
+            display:block;
+            border: 2px solid transparent;
+            width: 19rem;
+            height: 3.5vh;
+            padding-left: 0.8em;
+            outline: none;
+            overflow: hidden;
+            background-color: #F3F3F3;
+            border-radius: 10px;
+            transition: all 0.5s;
+        }
+        </style>`
+        return template
+    }
+
+    static get observedAttributes(){
+        return ["estado"]
+        
+    }
+    cambiartexto(newValue){
+        this.message=document.createElement("h1")
+        this.shadowRoot.appendChild(this.message)
+        
+        if(newValue===1){
+            this.labelC.textContent="Todo bien"
+        }else{
+            
+            this.labelC.textContent="Tienes un error al ingresar datos en usuario"
+        }
+        
+        
+    }
+
+    attributeChangedCallback(name, oldValues,newValue){
+        console("lee estos")
+        //this.cambiartexto(newValue)
+        
+    }
+
+
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+    connectedCallback(){
+//getters     
+        let labelP= this.getAttribute("data-label")
+        let placeholder=this.getAttribute("placeholder")
+        let type = this.getAttribute("type")
+        let value = this.getAttribute("value")
+        let diseno= this.getAttribute("diseno")
+        let id= this.getAttribute("id")
+//setters
+        this.inputC.setAttribute("id",id)
+        this.inputC.setAttribute("placeholder", placeholder)
+        this.inputC.setAttribute("type",type)
+        this.inputC.setAttribute("value", value)
+        this.inputC.setAttribute("class",diseno)
+        
+        this.labelC.textContent=labelP
+
+        this.shadowRoot.appendChild(this.labelC)
+        this.shadowRoot.appendChild(this.inputC)
+
+        this.render()
+        
+    }
+}
+
+customElements.define("my-input",inputComponent)//cambiar el nombre a input
+//input-component
+
+
---
+++ src/components/message/message-component.js
@@ -0,0 +1,61 @@
+class message extends HTMLElement{
+    //constructor
+    constructor(){
+        super()
+        this.attachShadow({mode: "open"})
+        this.h1=document.createElement("h1")//cambiar el nombre de la variable elementH1
+    }
+
+    getTemplate(){
+        const template=document.createElement("template")
+        template.innerHTML=
+        
+        `<style>
+        h1{
+            text-align:center;
+            align-items:center;
+            font-size: 1rem;
+            line-height: 1.25rem;
+            color: rgb(255 255 255);
+        }
+        </style>`
+        return template
+    }
+
+    static get observedAttributes(){
+        return ["estado", "mensaje"] 
+        //[estado,mensaje] [bien, todo bien] crear un objeto con los dos atributos
+    }
+
+    cambiartexto(mensaje){
+        this.message=document.createElement("h1")
+        this.shadowRoot.appendChild(this.message)
+        console.log(pasa)
+        this.h1.innerHTML=mensaje
+    }
+   
+    attributeChangedCallback(name, oldValues,newValue){
+
+        if(name==="estado"){
+            if(newValue==="bien"){
+                this.h1.setAttribute("hidden",true)
+            }else{
+                this.h1.removeAttribute("hidden",false)
+            }     
+        }
+        else if(name==="mensaje"){
+            this.h1.innerHTML=newValue
+        }    
+    }
+
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+    connectedCallback(){
+    this.h1.textContent=""
+    this.shadowRoot.appendChild(this.h1)
+    this.h1.setAttribute("style","color: white")
+    this.h1.setAttribute("hidden",true)
+    }
+}
+customElements.define("my-message",message) //message-componet
